e85eca9a5dfe1cf713a2d5a60c4e53da8412498e
jenmlucas <jenmlucas1@gmail.com>
2021-10-19 15:22:21 -0700

    got data by lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,47 +19,50 @@ var cityInput = document.getElementById("city");
 var cityForm = document.getElementById("city-form")
 var weatherContainer = document.getElementById("weather-container");
 var weatherSearchTerm = document.getElementById("city-search-term");
-// var latitude = 37.0902;
-// var longitude = -95.7129;
 var weather = document.getElementById("display-forecast");
+// var citySearch = latitude + longitude;
+// var latitude = "";
+// var longitude= "";
 
-var getCity = function (city) {
-    var apiList = "https://api.openweathermap.org/data/2.5/onecall?&lat=37.0902&lon=-95.7129&exclude=minutely,hourly&appid=5c71643f7754882962dd3859f2f84f94&units=imperial"
+var getCity = function (latitude, longitude) {
+    var apiList = "https://api.openweathermap.org/data/2.5/onecall?&lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly&appid=5c71643f7754882962dd3859f2f84f94&units=imperial"
+
     //"https://api.openweathermap.org/data/2.5/forecast?q=mesa&appid=5c71643f7754882962dd3859f2f84f94";
-    console.log("function was called");
+    // console.log("function was called");
     fetch(apiList).then(function (response) {
-        console.log(response);
+        // console.log(response);
         if (response.ok) {
             response.json().then(function (data) {
-                displayCity(data, city);
+                displayCity(data);
                 console.log(data);
-                
-                weather.innerHTML= "";
-            
+                weather.innerHTML = "";
+         
                 var currentTemp = data.current.temp;
-                console.log(currentTemp);
+                // console.log(currentTemp);
                 var temp = document.createElement("p")
-                temp.textContent= "Tempature" + " " + currentTemp;
-                weather.appendChild(temp);          
-              
+                temp.textContent = "Tempature" + " " + currentTemp + " " + "°F";
+                weather.appendChild(temp);
+
                 var currentHumidity = data.current.humidity;
-                console.log(currentHumidity);
+                // console.log(currentHumidity);
                 var humidity = document.createElement("p")
-                humidity.textContent= "Humidity" + " " + currentHumidity;
-                weather.appendChild(humidity);    
+                humidity.textContent = "Humidity" + " " + currentHumidity + " " + "%";
+                weather.appendChild(humidity);
 
                 var currentWindSpeed = data.current.wind_speed;
-                console.log(currentWindSpeed);
+                // console.log(currentWindSpeed);
                 var wind = document.createElement("p")
-                wind.textContent= "Wind-Speed" + " " +  currentWindSpeed;
-                weather.appendChild(wind);    
+                wind.textContent = "Wind-Speed" + " " + currentWindSpeed + " " + "MPH";
+                weather.appendChild(wind);
 
                 var currentUvi = data.current.uvi;
-                console.log(currentUvi);
+                // console.log(currentUvi);
                 var uvi = document.createElement("p")
-                uvi.textContent= "Uvi" + " " + currentUvi;
-                weather.appendChild(uvi);    
-                
+                uvi.textContent = "Uvi" + " " + currentUvi;
+                weather.appendChild(uvi);
+
+
+                // saveCityInfo();
             });
         } else {
             alert("Error: City Not Found");
@@ -70,41 +73,62 @@ var getCity = function (city) {
         })
 };
 
+var getCityName = function () {
+    var cityName = cityInput.value;
+    // console.log(cityName);
+    var apiCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=5c71643f7754882962dd3859f2f84f94";
+
+    fetch(apiCity).then(function (response) {
+        // console.log(response);
+        if (response.ok) {
+            response.json().then(function (data) {
+                // console.log(data);
+                var lat = data.coord.lat;
+                var lon= data.coord.lon;
+                // console.log(lat, lon)
+                getCity(lat, lon);
+            })
+        }
+    })
+};
+
+
 //to display city infor into correct areas 
 var displayCity = function (getCity, searchTerm) {
-    weatherContainer.textContent = "";
-    weatherSearchTerm.textContent = searchTerm;
-    console.log(getCity);
-    console.log(searchTerm);
-};
+        weatherContainer.textContent = "";
+        weatherSearchTerm.textContent = searchTerm;
+        
+        // console.log(getCity);
+        // console.log(searchTerm);
+        // reset(); 
+    };
+
+    // //save city to local storage
+    // var saveCityInfo = function() {
+    //  localStorage.setItem("apiList", JSON.stringify(apiList));
 
 
+    // }   
 
-// //save city to local storage
-// var saveCityInfo = function() {
-//     var weather = $(this).siblings(".displayForecast").val();
-//     var weatherContent = $(this).siblings(".displayForecast").attr("class");
+    // var getCityInfo =function() {
+    //  var savedSearches = localStorage.getItem("", ()); 
+    // }
 
-//     var storage = localStorage.setItem(weatherContent, JSON.stringify(weather));
+    //to use search button
+    var formSearchHandler = function (event) {
+        event.preventDefault();
+        // console.log(event);
 
+        var cityName = cityInput.value.trim();
 
-// }   
+        if (cityName) {
+            getCity(cityName);
+            cityInput.value = "";
+        } else {
+            alert("Please enter a city.")
+        }
+    };
 
-//to use search button
-var formSearchHandler = function (event) {
-    event.preventDefault();
-    console.log(event);
+    // getCity();
 
-    var cityName = cityInput.value.trim();
-
-    if (cityName) {
-        getCity(cityName);
-        cityInput.value = "";
-    } else {
-        alert("Please enter a city.")
-    }
-};
-
-getCity();
-
-search.addEventListener("click", formSearchHandler);
+    search.addEventListener("click", getCityName);
